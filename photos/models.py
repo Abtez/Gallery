acc778172231db623d29464f54a0fa0d90d07d67
Abzed <ibraabzed@gmail.com>
2020-10-10 13:25:51 +0300

    add location model

--- photos/models.py
+++ photos/models.py
@@ -4,7 +4,8 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=40)
     description = models.TextField()
-    tag = models.ForeignKey()
+    tags = models.ManyToManyField(Location)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE,)
     
     def __str__(self):
         return self.first_name
@@ -12,7 +13,18 @@ class Image(models.Model):
     def save_image(self):
         self.save()
         
+    @classmethod
+    def search_by_category(cls,search_term):
+        photos = cls.objects.filter(category__icontains=search_term)
+        return photos
+        
 class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
