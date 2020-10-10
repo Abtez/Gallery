9223fd7ac775a314b78da509e151b6218740331d
Abzed <ibraabzed@gmail.com>
2020-10-10 13:17:50 +0300

    add image and location models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=40)
+    description = models.TextField()
+    tag = models.ForeignKey()
+    
+    def __str__(self):
+        return self.first_name
+    
+    def save_image(self):
+        self.save()
+        
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+    
