a850a508a7d83df3f76a7b944ef65db286a24367
Abzed <ibraabzed@gmail.com>
2020-10-10 16:45:28 +0300

    add modal and such function

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('', views.main, name='mainPage'), 
+    path('search/', views.main, name='mainPage'), 
 ]
 
 if settings.DEBUG:
--- photos/views.py
+++ photos/views.py
@@ -5,4 +5,19 @@ from .models import *
 
 def main(request):
     images = Image.objects.all()
-    return render(request,'index.html',{'images':images})
+    category = Category.objects.all()
+    context = {'images':images, 'category':category}
+    return render(request,'index.html',context)
+
+def search_images(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_category = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"image": searched_category})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
+    
