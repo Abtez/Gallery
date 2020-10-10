3b212a521d083b9cd31948958d5ee02caeef71dc
Abzed <ibraabzed@gmail.com>
2020-10-10 13:02:47 +0300

    add navbar import admin and path to app

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse,Http404
+from .models import *
 
-# Create your views here.
+
+def main(request):
+    return render(request,'index.html')
